4cc30a92aa7c270c786d1bc4d10874cb34b598e4
Kyuhyoung Choi <kyuhyoung@gmail.com>
2023-07-03 15:58:51 +0900

    Add usage.

---
+++ usage.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\GNU Octave\Octave-8.2.0\octave-launch.exe" --gui
+Rem "C:\Program Files\GNU Octave\Octave-8.2.0\mingw64\bin\octave.bat" "run calib_gui.m" 
+Rem "C:\Program Files\GNU Octave\Octave-8.2.0\mingw64\bin\octave.exe" "calib_gui.m" 
